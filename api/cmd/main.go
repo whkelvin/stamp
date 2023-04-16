b0a876b517ac15d52915f91e264593ba104bcafb
whkelvin <whkelvin.hsu@gmail.com>
2023-04-16 16:42:54 -0300

    commented out db conn to prepare for initial deployment

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"context"
-	"github.com/jackc/pgx/v5"
+	//"context"
+	//"github.com/jackc/pgx/v5"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
 	. "github.com/whkelvin/stamp/api/config"
-	. "github.com/whkelvin/stamp/api/features/write_post/controller"
-	. "github.com/whkelvin/stamp/features/pkg/write_post"
+	//. "github.com/whkelvin/stamp/api/features/write_post/controller"
+	//. "github.com/whkelvin/stamp/features/pkg/write_post"
 	"net/http"
 )
 
@@ -25,20 +25,20 @@ func main() {
 		return key == config.ApiKey, nil
 	}))
 
-	conn, err := pgx.Connect(context.Background(), config.PostgresConnectionString)
-	if err != nil {
-		log.Fatal("Postgres Connection Failed.")
-		log.Fatal(err.Error())
-	}
-	defer conn.Close(context.Background())
+	//conn, err := pgx.Connect(context.Background(), config.PostgresConnectionString)
+	//if err != nil {
+	//	log.Fatal("Postgres Connection Failed.")
+	//	log.Fatal(err.Error())
+	//}
+	//defer conn.Close(context.Background())
 
 	var baseUrl = "/api/v1"
 
 	e.GET(baseUrl+"/health", healthCheck)
 
-	writePostFeature := &WritePostFeature{Database: conn}
-	var writePostController *WritePostController = &WritePostController{Handler: writePostFeature.Init()}
-	writePostController.Init(baseUrl+"/post", e)
+	//writePostFeature := &WritePostFeature{Database: conn}
+	//var writePostController *WritePostController = &WritePostController{Handler: writePostFeature.Init()}
+	//writePostController.Init(baseUrl+"/post", e)
 
 	e.Logger.Fatal(e.Start(config.Port))
 }
