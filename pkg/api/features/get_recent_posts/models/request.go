be0475c456fc1231e043bd512c1c7fc55ac1bb31
whkelvin <whkelvin.hsu@gmail.com>
2023-04-28 00:10:12 -0300

    removed comments

--- pkg/api/features/get_recent_posts/models/request.go
+++ pkg/api/features/get_recent_posts/models/request.go
@@ -4,9 +4,4 @@ import (
 	"github.com/whkelvin/stamp/pkg/api/generated/models"
 )
 
-//type Request struct {
-//	Size int `query:"size"`
-//	Page int `query:"page"`
-//}
-
 type Request = models.GetRecentPostsParams
--- pkg/api/features/write_post/models/request.go
+++ pkg/api/features/write_post/models/request.go
@@ -4,11 +4,4 @@ import (
 	"github.com/whkelvin/stamp/pkg/api/generated/models"
 )
 
-//type Request struct {
-//	Link        string `json:"link"`
-//	Title       string `json:"title"`
-//	Description string `json:"description"`
-//	RootDomain  string `json:"rootDomain"`
-//}
-
 type Request = models.PostPostRequest
